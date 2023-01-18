21ee8090643e450649c886658fdc87ac14104782
Rahul Saini <rscoder7@outlook.com>
2023-01-18 22:29:37 +1100

    Finished tests 01-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  celsius = ((farenheit - 32) * 5/9);
+  return celsius % 1 !== 0 ? parseFloat(celsius.toFixed(1)) : parseFloat(celsius.toFixed(0));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  farenheit =((celsius * 9/5) + 32);
+  return farenheit % 1 !== 0 ? parseFloat(farenheit.toFixed(1)) : parseFloat(farenheit.toFixed(0));
 };
 
 // Do not edit below this line
