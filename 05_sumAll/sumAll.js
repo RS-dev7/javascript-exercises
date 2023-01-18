44d295dd4c0e557d70781d6fe90b8e038e4e9b6a
Rahul Saini <rscoder7@outlook.com>
2023-01-18 22:13:11 +1100

    Ex 1-5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+
+    for (let i = num1; num1 < num2 ? i <= num2 : i >= num2; num1 > num2 ? i-- : i++) {
+        sum += i;
+    }
+
+    return num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number' ? 'ERROR' : sum;
 };
 
 // Do not edit below this line
